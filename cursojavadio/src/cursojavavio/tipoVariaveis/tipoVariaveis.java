e0a4d15e5ee4584700a6d4144fb75f1657f7a03b
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-02-23 01:43:41 -0300

    segundo commit atividade java com eclipse

---
+++ cursojavadio/src/cursojavavio/tipoVariaveis/tipoVariaveis.java
@@ -0,0 +1,26 @@
+package cursojavavio.tipoVariaveis;
+
+public class tipoVariaveis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double salarioMinimo = 2500;
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short) numeroNormal;
+		
+		System.out.println("O salario minimo é: " + salarioMinimo);
+
+		System.out.println("O salario minimo é: " + numeroCurto);
+
+		System.out.println("O salario minimo é: " + numeroNormal);
+		
+
+		System.out.println("O salario mminimo é: " + numeroCurto2);
+
+		
+
+	}
+
+}
